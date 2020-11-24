eae996d985a3419ce0e9bc0c3ca69f4fb6abf983
erika-goodwin <shellvoerika@gmail.com>
2020-11-24 12:38:27 +0900

    L45

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -611,3 +611,44 @@ console.log(myCountry);
 
 */
 
+//L45 CODING CHALLENGE
+
+//1
+const mark = {
+    fullName: 'Mark Miller',
+    mass: 78,
+    height: 1.68,
+    calcBmi: function () {
+        this.bmi = this.mass / (this.height ** 2);
+        return this.bmi;
+    }
+};
+const john = {
+    fullName: 'John Smith',
+    mass: 92,
+    height: 1.95,
+    calcBmi: function () {
+        this.bmi = this.mass / (this.height ** 2);
+        return this.bmi;
+    }
+};
+
+
+//MY ATTEMPT
+// console.log(mark.calcBmi());
+// console.log(john.calcBmi());
+
+// console.log(mark.calcBmi() < john.calcBmi ? `${john.fullName}'s BMI (${john.calcBmi()}) is higher than ${mark.fullName}’s (${mark.calcBmi()})` : `${mark.fullName}'s BMI (${mark.calcBmi()}) is higher than ${john.fullName}’s (${john.calcBmi()})`);
+
+
+//TEACHERS ATTEMPT
+mark.calcBmi();
+john.calcBmi();
+
+console.log(mark.bmi, john.bmi);
+
+if (mark.bmi > john.bmi) {
+    console.log(`${john.fullName}'s BMI (${john.calcBmi()}) is higher than ${mark.fullName}’s (${mark.calcBmi()})`);
+} else {
+    console.log(`${mark.fullName}'s BMI (${mark.calcBmi()}) is higher than ${john.fullName}’s (${john.calcBmi()})`);
+};
