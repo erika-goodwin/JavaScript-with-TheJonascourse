b972759ec69324697bc870765b26bdbf03434c82
erika-goodwin <shellvoerika@gmail.com>
2020-11-28 08:25:09 +0900

    L48

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -761,3 +761,26 @@ console.log(percentages2);
 */
 
 // L48
+const jonas = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Micheal', 'Peter', 'Steven'],
+    true
+];
+
+//loop backwards
+//4, 3, 2, 1 ...
+for (let i = jonas.length - 1; i >= 0; i--) {
+    console.log(i, jonas[i]);
+};
+
+//a loop inside of a loop
+for (let exercise = 1; exercise < 4; exercise++) {
+    console.log(`-----STARTING EXERCISE ${exercise} -----`);
+
+    for (let rep = 1; rep <= 5; rep++) {
+        console.log(`Exercise${exercise}: LIFTING WEIGHT REPETION ${rep}`)
+    }
+};
