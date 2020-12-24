a4008a6d5469379ff2e98d26edc5701159f2e7c0
erika-goodwin <shellvoerika@gmail.com>
2020-12-24 13:10:55 +0900

    L72

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,1 +1,28 @@
 'use strict';
+/*
+//L70
+//GETTING THE CONTENT OF THE ELEMENT
+console.log(document.querySelector('.message')); //All line from HTML
+console.log(document.querySelector('.message').textContent); //Only text content from HTML
+
+//L72
+//SETTING THE CONTENNT OF THE ELEMENT
+document.querySelector('.message').textContent = 'Correct Number!';
+
+//SCORE
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+//SELECTOR
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value); //.value: propaty
+*/
+
+//L73
+//mouse click, mouse moving , key press ,,,
+//wait to react with a event listener
+
+document.querySelector('.check').addEventListener('click', function () {
+  //.addEventListener = method
+  console.log(document.querySelector('.guess').value);
+}); //click = the first event  //function = just value
