b803e4895fb3e721fbc4ae4374efd9f7f50fae0e
erika-goodwin <shellvoerika@gmail.com>
2021-02-11 20:35:48 +0900

    L81

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -7,9 +7,23 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
+//SHOW MODAL
+const openModal = function () {
+  console.log('Buttun clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('Buttun clicked');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+//CLOSE MODAL
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+//CLOSE MODAL with X bottun
+btnCloseModal.addEventListener('click', closeModal);
+//CLOSE MODAL with clicking
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function);
